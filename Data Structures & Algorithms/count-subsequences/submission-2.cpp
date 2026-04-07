ca20685589ed062d7cc116a4668cfadfc7d206a6
Ashwin P <ashwinprasanna9@gmail.com>
2026-04-07 19:17:13 +0530

    Add: count-subsequences - submission-2

---
+++ Data Structures & Algorithms/count-subsequences/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n1 = s.length(), n2 = t.length();
+        if(n2 > n1) return 0;
+
+        vector<vector<int>> dp(s.length(), vector<int> (t.length(),-1));
+        return helper(0,0,s,t,dp);
+    }
+
+    int helper(int idx, int j, string s, string t, vector<vector<int>> &dp){
+        if(j == t.length()){
+            return 1;
+        }
+
+        if(idx >= s.length()) return 0;
+
+        if(dp[idx][j] != -1) return dp[idx][j];
+        dp[idx][j] = 0;
+        for(int i = idx;i<s.length();i++){
+            if(s[i] == t[j]){
+                dp[idx][j] += helper(i+1,j+1,s,t,dp);
+            }
+        }
+
+        return dp[idx][j];
+    }
+};
